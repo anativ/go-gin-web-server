14b7836c90471be8e93c400d7538201507fa10c5
anativ <alon.nativ@tomorrow.io>
2024-02-10 11:31:51 +0200

    connect to DB

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"os"
@@ -10,11 +11,50 @@ import (
 )
 
 func main() {
-	pgURL := os.Getenv("PG_URL")
-	fmt.Printf("PG_URL: %s\n", pgURL)
+
 	ConfigRuntime()
 	StartWorkers()
 	StartGin()
+}
+
+func ConnectDB() {
+	dbURI := os.Getenv("PG_URL")
+	db, err := sql.Open("postgres", dbURI)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// Check if the connection is successful
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Successfully connected!")
+
+	rows, err := db.Query("SELECT id, key, val FROM db1")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var id int
+		var key, val string
+
+		err := rows.Scan(&id, &key, &val)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("ID: %d, key: %s, val: %s\n", id, key, val)
+	}
+
+	// Check for errors from iterating over rows
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 // ConfigRuntime sets the number of operating system threads.
